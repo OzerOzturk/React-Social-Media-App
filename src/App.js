2f6c791ed19da0300b535b6048897a415c210534
OzerOzturk <ozerozturk1636@gmail.com>
2021-12-17 00:41:55 +0300

    login and register pages added. social media app ui design completed.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Home from "./pages/home/Home";
-import Profile from "./pages/profile/Profile";
 
 
 function App() {
   return (
     <>
-      <Profile/> 
-
+      <Home/>
     </>
   ) 
 }
