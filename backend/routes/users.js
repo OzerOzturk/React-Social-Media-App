90e49a4cddd7b5d918dcca9984510d6ec76586d1
OzerOzturk <ozerozturk1636@gmail.com>
2021-12-19 00:23:36 +0300

    login and register system added.

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,5 @@
+const User = require("../models/User")
 const router = require("express").Router();
 
-router.get("/", (req,res) => {
-    res.send("hey its user route")
-})
 
-module.exports = router
+module.exports = router;
