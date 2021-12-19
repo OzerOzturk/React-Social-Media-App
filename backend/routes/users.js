c9a7d489ff262a8b30f32d79c2f8291c1fea0d03
OzerOzturk <ozerozturk1636@gmail.com>
2021-12-19 13:32:57 +0300

    Revert "login and register system added."
    This reverts commit 90e49a4cddd7b5d918dcca9984510d6ec76586d1.

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,7 @@
-const User = require("../models/User")
 const router = require("express").Router();
 
+router.get("/", (req,res) => {
+    res.send("hey its user route")
+})
 
-module.exports = router;
+module.exports = router
